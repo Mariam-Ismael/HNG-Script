497dcbb982b4094458d6da2feed5d040d888cb52
Mariam-Ismael <65224611+Mariam-Ismael@users.noreply.github.com>
2020-06-03 21:58:28 +0100

    Update Mariam-Ismael-Saka.js

--- scripts/Mariam-Ismael-Saka.js
+++ scripts/Mariam-Ismael-Saka.js
@@ -1,5 +1,5 @@
 var mystring1 = "Hello World, ";
-var mystring2 = "this is Mariam Ismael Saka with HNG ID ";
+var mystring2 = "this is Mariam Ismael Saka with HNGi7 ID ";
 var mystring3 = "HNG-01795 and email ";
 var mystring4 = "mariamismael904@gmail.com ";
 var mystring5 = "using Javascript for stage 2 task. ";
